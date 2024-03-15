906811c712865e877d8bc7259f011f4dedfdd99e
ChuMutt <chufilthymutt@gmail.com>
2024-03-15 13:47:05 -0500

    much better

--- .ccls-cache/@home@chu@projects@programming@mal/impls@cpp@step0_repl.cpp
+++ .ccls-cache/@home@chu@projects@programming@mal/impls@cpp@step0_repl.cpp
@@ -2,38 +2,25 @@
 #include <string>
 using namespace std;
 
-string READ()
-{
-  string read = "";
-  cin >> read;
-  return read;
-}
-
-string EVAL()
-{
-  string eval = "";
-  eval = READ();
-  return eval;
-}
-
-string PRINT()
-{
-  string print = "";
-  print = EVAL();
-  return print;
-}
-
-string rep()
-{
-  string rep = "";
-  rep = PRINT();
-  return rep;
+string rep() {
+  cout << "user> ";
+  string input;
+  getline(cin, input);
+  cout << endl;
+  cout << input << endl;
+  return input;
 }
 
 int main()
 {
-  string repOut = rep();
-  cout << repOut << endl;
-
+  while (true)
+  {
+    rep();
+    if (cin.eof())
+    {
+      break;
+    }
+    cin.clear();
+  }
   return 0;
 }

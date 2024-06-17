c6d7528a3ab18328045accb71e1a908c177e1771
ChuMutt <chufilthymutt@gmail.com>
2024-06-17 01:07:04 -0500

    types fix

--- impls/cpp/src/types.cpp
+++ impls/cpp/src/types.cpp
@@ -4,8 +4,12 @@ std::string ListValue::inspect() {
   std::string out = "(";
   for (auto *value : m_list) {
     out.append(value->inspect());
+    out.append(" ");
   }
-  out.append(")");
-  out[out.length() - 1] = ')';
+  if (m_list.size() > 0) {
+    out[out.length() - 1] = ')';
+  } else {
+    out.append(")");
+  }
   return out;
 }

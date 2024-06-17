0debf3a0d7bd552f6d1916f5066019ea25bebf6a
ChuMutt <chufilthymutt@gmail.com>
2024-06-17 09:55:19 -0500

    work on main

--- impls/cpp/src/step1_read_print.cpp
+++ impls/cpp/src/step1_read_print.cpp
@@ -12,24 +12,34 @@ Value *EVAL(Value *s) { return s; }
 std::string PRINT(Value *s) { return pr_str(s, true); }
 
 std::string rep(std::string s) {
-  auto ast = READ(s);
-  auto result = EVAL(ast);
-  return PRINT(result);
+  // auto ast = READ(s);
+  // auto result = EVAL(ast);
+  // return PRINT(result);
+  try {
+    auto ast = READ(s);
+    auto result = EVAL(ast);
+    return PRINT(result);
+  } catch (ExceptionValue *exception) {
+    std::cerr << exception->message() << std::endl;
+    return "";
+  }
 }
 
 int main() {
   const auto history_path = "history.txt";
   linenoise::LoadHistory(history_path);
+
   std::string s;
   for (;;) {
     auto quit = linenoise::Readline("user> ", s);
-    linenoise::AddHistory(s.c_str());
     if (quit) { // ctrl+d to quit
       break;
     }
     std::cout << rep(s) << std::endl;
-    std::cin.clear();
+    linenoise::AddHistory(s.c_str());
   }
+
   linenoise::SaveHistory(history_path);
+
   return 0;
 }
